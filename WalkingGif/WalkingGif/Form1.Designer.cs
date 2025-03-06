179183fb1766770ace3ecbb74be78c6d7252df17
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-03-06 14:17:49 +0300

    Walking Gif project is added

---
+++ WalkingGif/WalkingGif/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WalkingGif
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WalkingGif.Properties.Resources.Adsız_tasarım;
+            this.pictureBox2.Location = new System.Drawing.Point(28, 202);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(120, 204);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WalkingGif.Properties.Resources.Adsız_tasarım__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(649, -5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 223);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Pink;
+            this.panel1.Location = new System.Drawing.Point(3, 387);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 22);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Pink;
+            this.panel2.Location = new System.Drawing.Point(283, 387);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(220, 22);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Pink;
+            this.panel3.Location = new System.Drawing.Point(565, 387);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(250, 22);
+            this.panel3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ WalkingGif/WalkingGif/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WalkingGif
+{
+    public partial class Form1 : Form
+    {
+        private Panel[] platforms; 
+        private int platformSpeed = 5; 
+        private int platformCount = 3; 
+        private Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePlatforms();
+            StartTimer();
+        }
+
+        private void InitializePlatforms()
+        {
+           
+            platforms = new Panel[] { panel1, panel2, panel3 };
+        }
+
+        private void StartTimer()
+        {
+            timer = new Timer
+            {
+                Interval = 50 
+            };
+            timer.Tick += MovePlatforms;
+            timer.Start();
+        }
+
+        private void MovePlatforms(object sender, EventArgs e)
+        {
+            for (int i = 0; i < platforms.Length; i++)
+            {
+                
+                platforms[i].Left -= platformSpeed;
+
+                
+                if (platforms[i].Right < 0)
+                {
+                    platforms[i].Left = this.ClientSize.Width;
+                }
+            }
+        }
+    }
+}
