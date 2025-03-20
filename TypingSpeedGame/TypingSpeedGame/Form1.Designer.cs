03994c48ec0103aa8aa261b3de26d23f1fcf96a3
Ecem Nur Özen <147251928+ecemoz@users.noreply.github.com>
2025-03-20 16:38:50 +0300

    new project is added.

---
+++ TypingSpeedGame/TypingSpeedGame/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TypingSpeedGame
+{
+    partial class GameForm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Location = new System.Drawing.Point(55, 185);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(50, 16);
+            this.lblWord.TabIndex = 0;
+            this.lblWord.Text = "WORD";
+            this.lblWord.Click += new System.EventHandler(this.lblWord_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(158, 182);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(55, 251);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(56, 16);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "Score: 0";
+            this.lblScore.Click += new System.EventHandler(this.lblScore_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(174, 251);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(82, 16);
+            this.lblTimer.TabIndex = 3;
+            this.lblTimer.Text = "Time Left: 30";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(523, 365);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblWord);
+            this.Name = "GameForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ TypingSpeedGame/TypingSpeedGame/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TypingSpeedGame
+{
+    public partial class GameForm : Form
+    {
+        private List<string> words = new List<string> { "computer", "keyboard", "monitor", "developer", "programming", "software", "hardware", "internet", "security", "network" };
+        private string currentWord;
+        private int score = 0;
+        private int timeLeft = 30; // 30 saniyelik oyun süresi
+        private Random random = new Random();
+
+        public GameForm()
+        {
+            InitializeComponent();
+            GenerateRandomWord();
+            this.KeyPreview = true;
+            txtInput.KeyDown += new KeyEventHandler(TxtInput_KeyDown);
+            gameTimer.Interval = 1000; // Her saniye çalışacak
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Start();
+        }
+
+        private void TxtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && timeLeft > 0)
+            {
+                if (txtInput.Text.Trim().ToLower() == currentWord.ToLower())
+                {
+                    score += 10; // Doğru cevap için 10 puan
+                    lblScore.Text = "Score: " + score;
+                    GenerateRandomWord();
+                }
+                else
+                {
+                    score -= 5; // Yanlış cevap için 5 puan düş
+                    lblScore.Text = "Score: " + score;
+                }
+                txtInput.Clear();
+            }
+        }
+
+        private void GenerateRandomWord()
+        {
+            currentWord = words[random.Next(words.Count)];
+            lblWord.Text = currentWord;
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            timeLeft--;
+            lblTimer.Text = "Time Left: " + timeLeft;
+
+            if (timeLeft <= 0)
+            {
+                gameTimer.Stop();
+                txtInput.Enabled = false;
+                MessageBox.Show("Game Over! Your Score: " + score);
+            }
+        }
+
+        private void lblScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblWord_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
